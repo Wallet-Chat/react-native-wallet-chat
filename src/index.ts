7d4a6bd83a1200ca662b283814f2be60aacd9239
izuchukwu-eric <onukwubeizu@gmail.com>
2023-07-13 19:09:49 +0100

    working to fix import issue

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as utils from './utils';
+export * as types from './types';
+export { default as WalletChatWidget } from './WalletChat';
+// export { default as ChatWithOwner } from './src/ChatWithOwner'
+export { WalletChatProvider } from './context';
